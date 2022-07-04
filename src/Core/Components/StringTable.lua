063df552642c6484ad52c198fb4adfab90387f30
Ryan <107238211+Ryanbyte@users.noreply.github.com>
2022-07-04 22:26:29 +0600

    Create StringTable.lua

---
+++ src/Core/Components/StringTable.lua
@@ -0,0 +1,9 @@
+local StringTypes = {
+	StringValue = Instance.new("StringValue"),
+	BoolValue = Instance.new("BoolValue"),
+	NumberValue = Instance.new("NumberValue"),
+	IntValue = Instance.new("IntValue"),
+	Custom = Instance.new("StringValue"),
+}
+
+return StringTypes
