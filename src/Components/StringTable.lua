894b0e9ad89ba8c5f41fa730b3c48a0c266b5e28
Ryan <107238211+Ryanbyte@users.noreply.github.com>
2022-07-04 22:24:07 +0600

    Delete src directory

--- src/Components/StringTable.lua
+++
@@ -1,9 +0,0 @@
-local StringTypes = {
-	StringValue = Instance.new("StringValue"),
-	BoolValue = Instance.new("BoolValue"),
-	NumberValue = Instance.new("NumberValue"),
-	IntValue = Instance.new("IntValue"),
-	Custom = Instance.new("StringValue"),
-}
-
-return StringTypes
