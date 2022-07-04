32343a2074e131ad9daf3de5fe863b75f77b56fa
Ryan <107238211+Ryanbyte@users.noreply.github.com>
2022-07-04 22:28:39 +0600

    Create project.Info.lua

---
+++ src/Info/project.Info.lua
@@ -0,0 +1,12 @@
+local Info = {
+	_Version_ = 2.00,
+	Github = "https://github.com/TriumvirateStudios/Stringbyte",
+	Author = "@TriumvirateStudios",
+	Link = "@TriumvirateStudios/Stringbyte",
+	Source = "/ServerScriptService/App/Stringbyte",
+	File_Type = ".rbxm",
+	
+	Mit_License = "@TriumvirateStudios/Stringbyte/LICENSE.md (https://github.com/TriumvirateStudios/Stringbyte/blob/master/LICENSE.md)"
+}
+
+return Info
